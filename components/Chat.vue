9db03f7e8a7652c8006959febc69307fbab7856e
JonasThers <jonasthers1992@gmail.com>
2023-07-08 01:53:38 +0200

    Add starting point for chat

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>
-    chat goes here
-  </div>
+  <section class="chat">
+    <div class="chat__window">
+      <li v-for="(messageInChat, index) in messagesInChat" :key=index class="chat__message">
+        {{ messageInChat.content }}
+      </li>
+    </div>
+    <form @submit="submitMessage">
+      <input name="message" id="message" v-model="message" />
+      <button type="submit">Submit</button>
+      <p v-if="failure">Please type in a valid message</p>
+    </form>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'Chat'
+  name: 'Chat',
+  data() {
+    return {
+      message: '',
+      messagesInChat: [],
+      failure: false,
+    }
+  },
+  methods: {
+    submitMessage(e) {
+      e.preventDefault()
+
+      if (!this.message) {
+        this.failure = true
+        return
+      }
+
+      this.failure = false
+
+      this.messagesInChat.push(
+        {
+          content: this.message,
+          sender: 'user'
+        }
+      )
+      this.message = '';
+    }
+  }
 }
 </script>
+
+<style scoped>
+.chat__window {
+  height: 500px;
+  width: 320px;
+  border: 1px solid black;
+  overflow-y: auto;
+}
+
+.chat__message {
+  list-style-type: none;
+}
+</style>
